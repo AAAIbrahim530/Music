9b14efe55d9a2010d9ba0bdfadc916f282e46c28
AAAIbrahim530 <07attiaibrahim@gmail.com>
2026-05-11 20:10:19 +0200

    linking ids to xml

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -11,11 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        var song1 = Song(  "Soft Life",  "Nomfundo Moh",
-                         2022,  "4:08")
-        var song2 = Song( "10,000 days (wings Pt2)", "Tool", 2006, "11:30")
-        var song3 = Song( "Just a little tune",  "Some Birdy Tweeted")
+        val song1 = Song("Soft Life", "Nomfundo Moh", 2022, "4:08")
+        val song2 = Song("10,000 days (wings Pt2)", "Tool", 2006, "11:30")
+        val song3 = Song("Just a little tune", "Some Birdy Tweeted")
+
         setContentView(R.layout.activity_main)
+
+        // Find views and set initial song data
+        val titleView = findViewById<android.widget.TextView>(R.id.songTitle)
+        val artistView = findViewById<android.widget.TextView>(R.id.songArtist)
+        
+        titleView.text = song1.name
+        artistView.text = song1.artist
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
