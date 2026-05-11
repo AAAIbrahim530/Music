2b5d499b62ffd540f6762545c13e29c1a15ec148
AAAIbrahim530 <07attiaibrahim@gmail.com>
2026-05-11 19:37:28 +0200

    adding songs

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -10,6 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        var song1 = Song(songName: "Soft Life", songArtist: "Nomfundo Moh"
+                         songYear: 2022, songDuration: "4:08")
+        var song2 = Song( songName = "10,000 days (wings Pt2)", songArtist = "Tool",
+                          songYear = 2006, songDuration = "11:13")
+        var song3 = Song(songName = "Just a little tune")
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
